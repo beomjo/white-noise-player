c8c5b87aeb3d891b1bac8021a25308cacd2e9151
beomjo <qjatjr1108@gmail.com>
2021-02-28 03:35:35 +0900

    feat: LoginActivity추가, LoginViewModel 바인딩

---
+++ app/src/main/java/com/beomjo/whitenoise/ui/auth/LoginActivity.kt
@@ -0,0 +1,8 @@
+package com.beomjo.whitenoise.ui.auth
+
+import com.beomjo.whitenoise.R
+import com.beomjo.whitenoise.base.BaseActivity
+import com.beomjo.whitenoise.databinding.ActivityLoginBinding
+
+class LoginActivity : BaseActivity<ActivityLoginBinding>(R.layout.activity_login) {
+}
---
+++ app/src/main/java/com/beomjo/whitenoise/ui/auth/LoginViewModel.kt
@@ -0,0 +1,5 @@
+package com.beomjo.whitenoise.ui.auth
+
+import com.beomjo.whitenoise.base.BaseViewModel
+
+class LoginViewModel : BaseViewModel()
