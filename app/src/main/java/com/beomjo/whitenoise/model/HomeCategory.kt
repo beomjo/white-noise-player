0bcd0c175f056125e6bceb6908ada95676885d4c
beomjo <qjatjr1108@gmail.com>
2021-03-15 02:37:45 +0900

    feat : Add Parcelable Annotation to HomeCategory Model

--- app/src/main/java/com/beomjo/whitenoise/model/HomeCategory.kt
+++ app/src/main/java/com/beomjo/whitenoise/model/HomeCategory.kt
@@ -1,14 +1,17 @@
 package com.beomjo.whitenoise.model
 
 import android.graphics.Color
+import android.os.Parcelable
+import kotlinx.android.parcel.Parcelize
 
+@Parcelize
 data class HomeCategory(
     val id: Int = 1,
     val name: String = "",
     val iconUrl: String = "",
     val colorPrimary: String = "",
     val colorSecondary: String = "",
-) {
+) : Parcelable {
 
     fun getPrimaryColor(): Int {
         return colorPrimary.parseColor()
