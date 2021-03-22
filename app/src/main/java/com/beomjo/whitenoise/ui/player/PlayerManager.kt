46216bcee306f6f154e5a04fd5971b8dba8bdd54
beomjo <qjatjr1108@gmail.com>
2021-03-23 00:03:25 +0900

    feat: hasData 지연 처리 추가

--- app/src/main/java/com/beomjo/whitenoise/ui/player/PlayerManager.kt
+++ app/src/main/java/com/beomjo/whitenoise/ui/player/PlayerManager.kt
@@ -1,15 +1,20 @@
 package com.beomjo.whitenoise.ui.player
 
 import androidx.databinding.BaseObservable
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
-import androidx.lifecycle.Transformations
+import androidx.lifecycle.*
 import com.beomjo.whitenoise.model.Sound
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 import javax.inject.Inject
 import javax.inject.Singleton
 
 @Singleton
 class PlayerManager @Inject constructor() : BaseObservable() {
+
+    private val playerScope = CoroutineScope(Dispatchers.Default)
 
     private val _state = MutableLiveData<SoundState>()
     val state: LiveData<SoundState> get() = _state
@@ -17,12 +22,14 @@ class PlayerManager @Inject constructor() : BaseObservable() {
     private val _sound = MutableLiveData<Sound?>()
     val sound: LiveData<Sound?> get() = _sound
 
-    val hasData: LiveData<Boolean> get() = Transformations.map(_sound) { it != null }
+    private val _hasData = MutableLiveData<Boolean>().apply { value = false }
+    val hasData: LiveData<Boolean> get() = _hasData
 
     private val _moveToPlayerActivity = MutableLiveData<Sound>()
     val moveToPlayerActivity: LiveData<Sound> get() = _moveToPlayerActivity
 
     fun setSound(sound: Sound) {
+        playerScope.launch { delayWith { _hasData.postValue(true) } }
         _sound.value = sound
     }
 
@@ -36,10 +43,18 @@ class PlayerManager @Inject constructor() : BaseObservable() {
 
     fun onClose() {
         _sound.value = null
+        _hasData.value = false
     }
 
     fun onExpand() {
         _moveToPlayerActivity.value = sound.value
+    }
+
+    private suspend fun <T> delayWith(block: () -> T): T {
+        return withContext(Dispatchers.IO) {
+            delay(450L)
+            block()
+        }
     }
 }
 
