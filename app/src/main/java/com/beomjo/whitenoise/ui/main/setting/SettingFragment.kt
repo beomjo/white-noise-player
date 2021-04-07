663a6ba6c6ea02c44956b104e852ad49ea6e69e3
beomjo <qjatjr1108@gmail.com>
2021-04-08 00:26:30 +0900

    feat: Email Contract 기능 추가

--- app/src/main/java/com/beomjo/whitenoise/ui/main/setting/SettingFragment.kt
+++ app/src/main/java/com/beomjo/whitenoise/ui/main/setting/SettingFragment.kt
@@ -1,10 +1,15 @@
 package com.beomjo.whitenoise.ui.main.setting
 
+import android.content.ActivityNotFoundException
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.lifecycle.ViewModelStoreOwner
+import com.beomjo.whitenoise.BuildConfig
 import com.beomjo.whitenoise.R
 import com.beomjo.whitenoise.base.BaseFragment
 import com.beomjo.whitenoise.databinding.FragmentSettingBinding
@@ -24,6 +29,43 @@ class SettingFragment : BaseFragment<FragmentSettingBinding>(
     ): View {
         super.onCreateView(inflater, container, savedInstanceState)
         return binding { fragment = this@SettingFragment }.root
+    }
+
+    fun onPerformContractUs() {
+        val emailIntent = Intent(Intent.ACTION_SENDTO).apply {
+            flags = Intent.FLAG_GRANT_READ_URI_PERMISSION
+            flags = Intent.FLAG_GRANT_WRITE_URI_PERMISSION
+            putExtra(Intent.EXTRA_EMAIL, arrayOf(getString(R.string.developer_email)))
+            putExtra(Intent.EXTRA_SUBJECT, getString(R.string.email_subject))
+            putExtra(
+                Intent.EXTRA_TEXT,
+                """
+                AppVersion : ${BuildConfig.VERSION_NAME}
+                Device : ${android.os.Build.MODEL}
+                OS Version : ${android.os.Build.VERSION.SDK_INT}
+                Content : 
+                """.trimIndent()
+            )
+            data = Uri.parse("mailto:")
+        }
+
+        try {
+            startActivity(emailIntent)
+        } catch (ex: ActivityNotFoundException) {
+            Toast.makeText(
+                activity,
+                getString(R.string.error_not_fond_email_client),
+                Toast.LENGTH_SHORT
+            ).show()
+        }
+    }
+
+    fun onPerformTermOfService() {
+
+    }
+
+    fun onPerformPrivacyPolicy() {
+
     }
 
     fun onBackPressed() {
